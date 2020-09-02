ec1acf2f2c9711fcd90cc5040598605e0f5d77d9
César Augusto Macedo <cesar.macedo@sysmap.com.br>
2020-09-01 23:14:10 -0300

    ajust cors

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'http://localhost:8080',
+    origin: 'https://frontend-app-grades.herokuapp.com',
   })
 );
 
